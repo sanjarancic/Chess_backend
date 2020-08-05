7d0b0f56c381a133a52228bfb6a348b3101010cf
Sanja Rancic <sanjarancic0@gmail.com>
2020-08-05 22:06:40 +0200

    Add DATABASE_URL

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 POSTGRES = {
     'user': 'chess',
     'pw': 'chess',
@@ -6,7 +8,12 @@ POSTGRES = {
     'port': '5432',
 }
 
+local_connection = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+connection_string = os.environ['DATABASE_URL'] if 'DATABASE_URL' in os.environ else local_connection
+
+
 config = {
-    'SQLALCHEMY_DATABASE_URI': 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES,
+    'SQLALCHEMY_DATABASE_URI': connection_string,
     'SECRET_KEY': 'secret-key'
 }
